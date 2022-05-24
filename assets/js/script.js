8cf55983d2a615d08b8728127396966fa54cf070
Kansas Carver <97691873+khcarver1@users.noreply.github.com>
2022-05-24 16:57:18 -0600

    set margin for mouseover functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,56 +16,94 @@ headerUl.appendChild(headerLi2);
 headerNav.appendChild(headerUl);
 headerElem.appendChild(headerNav);
 
+
+//style
+//ul
+headerUl.style.display = "flex";
+headerUl.style.textAlign = "center";
+headerUl.style.listStyleType = "none";
+//Li A tags
+headerLi0A.style.color = "var(--secondary-color)";
+headerLi1A.style.color = "var(--secondary-color)";
+headerLi2A.style.color = "var(--secondary-color)";
+headerLi0A.style.textDecoration = "none";
+//Li
+headerLi0.style.textDecoration = "none";
+headerLi0.style.margin = "20px";
+headerLi1.style.margin = "20px";
+headerLi2.style.margin = "20px";
+//headerNav
+headerNav.style.justifyContent = "space-around";
+//headerElem
 headerElem.style.width = "700px";
 headerElem.style.display = "flex";
 headerElem.style.justifyContent = "end";
 headerElem.style.alignItems = "center";
-headerLi0.style.margin = "15px";
-headerLi1.style.margin = "15px";
-headerLi2.style.margin = "15px";
+
+
+//setattr
+headerLi0A.setAttribute("href", "./currentlistings.html");
 
 
 
-headerNav.style.justifyContent = "space-around";
-
-headerLi0A.style.color = "var(--secondary-color)";
-headerLi1A.style.color = "var(--secondary-color)";
-headerLi2A.style.color = "var(--secondary-color)";
-
-//Ul
-headerUl.style.display = "flex";
-headerUl.style.textAlign = "center";
-
-headerUl.style.listStyleType = "none";
-headerLi0A.style.textDecoration = "none";
-headerLi0.style.textDecoration = "none";
-
-
-//Li
+//Li nav item names
 headerLi0A.textContent = "Current Listings";
-
 headerLi1A.textContent = "Sell with Shelly";
 headerLi2A.textContent = "Testimonials";
 
-//setattr
-headerLi0A.setAttribute("href","www.ebay.com");
-
+//beehive logo mouseover functions
 headerLi0.addEventListener("mouseenter", moBeeHiveLi0A);
 headerLi0.addEventListener("mouseleave", mlBeeHiveLi0A);
-
 function moBeeHiveLi0A() {
-
     beeHiveLi0A = document.createElement('img');
-    beeHiveLi0A.setAttribute("src","./assets/favicon/beehivefavicon.png");
+    beeHiveLi0A.setAttribute("src", "./assets/favicon/beehivefavicon.png");
     beeHiveLi0A.style.width = "40px";
     beeHiveLi0A.style.height = "25px";
     headerLi0.style.display = "flex";
     headerLi0.style.alignItems = "center";
-
     headerLi0.appendChild(beeHiveLi0A);
-
+    headerLi0.insertBefore(headerLi0A, headerLi0.children[2]);
 };
 function mlBeeHiveLi0A() {
     headerLi0.removeChild(beeHiveLi0A);
 
+};
+headerLi1.addEventListener("mouseenter", moBeeHiveLi1A);
+headerLi1.addEventListener("mouseleave", mlBeeHiveLi1A);
+function moBeeHiveLi1A() {
+    beeHiveLi1A = document.createElement('img');
+    beeHiveLi1A.setAttribute("src", "./assets/favicon/beehivefavicon.png");
+    beeHiveLi1A.style.width = "40px";
+    beeHiveLi1A.style.height = "25px";
+    headerLi1.style.display = "flex";
+    headerLi1.style.alignItems = "center";
+    headerLi1.style.marginLeft = "0px";
+    headerLi0.style.marginRight = "0px";
+    headerLi1.appendChild(beeHiveLi1A);
+    headerLi1.insertBefore(headerLi1A, headerLi1.children[2]);
+};
+function mlBeeHiveLi1A() {
+    headerLi1.removeChild(beeHiveLi1A);
+    headerLi1.style.marginLeft = "20px";
+    headerLi0.style.marginRight = "20px";
+
+};
+headerLi2.addEventListener("mouseenter", moBeeHiveLi2A);
+headerLi2.addEventListener("mouseleave", mlBeeHiveLi2A);
+function moBeeHiveLi2A() {
+    beeHiveLi2A = document.createElement('img');
+    beeHiveLi2A.setAttribute("src", "./assets/favicon/beehivefavicon.png");
+    beeHiveLi2A.style.width = "40px";
+    beeHiveLi2A.style.height = "25px";
+    headerLi2.style.display = "flex";
+    headerLi2.style.alignItems = "center";
+    headerLi2.style.marginLeft = "0px";
+    headerLi1.style.marginRight = "0px";
+    headerLi2.appendChild(beeHiveLi2A);
+    headerLi2.insertBefore(headerLi2A, headerLi2.children[2]);
+};
+function mlBeeHiveLi2A() {
+    headerLi2.removeChild(beeHiveLi2A);
+    headerLi2.style.marginLeft = "20px";
+    headerLi1.style.marginRight = "20px";
 };
