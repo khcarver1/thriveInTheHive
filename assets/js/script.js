deeb61f1a5b97ef44ad0429929a3c15dc3a4941e
Kansas Carver <97691873+khcarver1@users.noreply.github.com>
2022-05-27 22:02:06 -0600

    nav shadow fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,9 +35,9 @@ headerLi2A.style.textShadow = "2px 2px 2px black";
 headerLi0A.style.textDecoration = "none";
 //Li
 headerLi0.style.textDecoration = "none";
-headerLi0.style.margin = "20px";
-headerLi1.style.margin = "20px";
-headerLi2.style.margin = "20px";
+headerLi0.style.margin = "30px";
+headerLi1.style.margin = "30px";
+headerLi2.style.margin = "30px";
 headerLi0.style.fontSize = "20px";
 headerLi1.style.fontSize = "20px";
 headerLi2.style.fontSize = "20px";
@@ -53,13 +53,10 @@ headerElem.style.alignItems = "center";
 //setattr
 headerLi0A.setAttribute("href", "./currentlistings.html");
 
-
-
 //Li nav item names
 headerLi0A.textContent = "Current Listings";
 headerLi1A.textContent = "Sell with Shelly";
 headerLi2A.textContent = "Testimonials";
-
 //beehive logo mouseover functions
 headerLi0.addEventListener("mouseenter", moBeeHiveLi0A);
 headerLi0.addEventListener("mouseleave", mlBeeHiveLi0A);
@@ -67,18 +64,18 @@ function moBeeHiveLi0A() {
     beeHiveLi0A = document.createElement('img');
     beeHiveLi0A.setAttribute("src", "./assets/favicon/beehivefavicon.png");
     beeHiveLi0A.style.width = "40px";
-    beeHiveLi0A.style.height = "25px";
+    beeHiveLi0A.style.height = "22.4px";
     headerLi0.style.display = "flex";
     headerLi0.style.alignItems = "center";
-    headerLi0.style.marginLeft = "-20px";
-
+    headerLi0.style.marginLeft = "-10px";
+    headerLi0A.style.textShadow = "2px 2px 2px grey";
     headerLi0.appendChild(beeHiveLi0A);
     headerLi0.insertBefore(headerLi0A, headerLi0.children[2]);
 };
 function mlBeeHiveLi0A() {
     headerLi0.removeChild(beeHiveLi0A);
-    headerLi0.style.marginLeft = "20px";
-
+    headerLi0.style.marginLeft = "30px";
+    headerLi0A.style.textShadow = "2px 2px 2px black";
 };
 headerLi1.addEventListener("mouseenter", moBeeHiveLi1A);
 headerLi1.addEventListener("mouseleave", mlBeeHiveLi1A);
@@ -86,19 +83,20 @@ function moBeeHiveLi1A() {
     beeHiveLi1A = document.createElement('img');
     beeHiveLi1A.setAttribute("src", "./assets/favicon/beehivefavicon.png");
     beeHiveLi1A.style.width = "40px";
-    beeHiveLi1A.style.height = "25px";
+    beeHiveLi1A.style.height = "22.4px";
     headerLi1.style.display = "flex";
     headerLi1.style.alignItems = "center";
-    headerLi1.style.marginLeft = "0px";
-    headerLi0.style.marginRight = "0px";
+    headerLi1.style.marginLeft = "10px";
+    headerLi0.style.marginRight = "10px";
+    headerLi1A.style.textShadow = "2px 2px 2px grey";
     headerLi1.appendChild(beeHiveLi1A);
     headerLi1.insertBefore(headerLi1A, headerLi1.children[2]);
 };
 function mlBeeHiveLi1A() {
     headerLi1.removeChild(beeHiveLi1A);
-    headerLi1.style.marginLeft = "20px";
-    headerLi0.style.marginRight = "20px";
-
+    headerLi1.style.marginLeft = "30px";
+    headerLi0.style.marginRight = "30px";
+    headerLi1A.style.textShadow = "2px 2px 2px black";
 };
 headerLi2.addEventListener("mouseenter", moBeeHiveLi2A);
 headerLi2.addEventListener("mouseleave", mlBeeHiveLi2A);
@@ -106,16 +104,18 @@ function moBeeHiveLi2A() {
     beeHiveLi2A = document.createElement('img');
     beeHiveLi2A.setAttribute("src", "./assets/favicon/beehivefavicon.png");
     beeHiveLi2A.style.width = "40px";
-    beeHiveLi2A.style.height = "25px";
+    beeHiveLi2A.style.height = "22.4px";
     headerLi2.style.display = "flex";
     headerLi2.style.alignItems = "center";
-    headerLi2.style.marginLeft = "0px";
-    headerLi1.style.marginRight = "0px";
+    headerLi2.style.marginLeft = "10px";
+    headerLi1.style.marginRight = "10px";
+    headerLi2A.style.textShadow = "2px 2px 2px grey";
     headerLi2.appendChild(beeHiveLi2A);
     headerLi2.insertBefore(headerLi2A, headerLi2.children[2]);
 };
 function mlBeeHiveLi2A() {
     headerLi2.removeChild(beeHiveLi2A);
-    headerLi2.style.marginLeft = "20px";
-    headerLi1.style.marginRight = "20px";
+    headerLi2.style.marginLeft = "30px";
+    headerLi1.style.marginRight = "30px";
+    headerLi2A.style.textShadow = "2px 2px 2px black";
 };
